53335ad73b3b4bf7404f68d9702d3ea93732553f
naereloire <naere.loire@gmail.com>
2020-08-24 15:14:13 -0300

    add babel rc

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
-// scripts aqui
+import express from 'express';
+import bodyParse from 'bodyParse';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.get('*', (req, res) =>
+  res.status(200).send('<html><h1>Hello World!</h1></html>'),
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
