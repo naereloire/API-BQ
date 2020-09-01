2c78267f9f4b20066183eba36f584416fe2e3496
naereloire <naere.loire@gmail.com>
2020-09-01 18:17:45 -0300

    udpate readme

---
+++ api/server/services/userService.js
@@ -0,0 +1,70 @@
+import database from '../src/models';
+
+class userService {
+  static async getAllUsers() {
+    try {
+      return await database.user.findAll();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async addUser(newuser) {
+    try {
+      return await database.user.create(newuser);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateUser(id, updateuser) {
+    try {
+      const userToUpdate = await database.user.findOne({
+        where: { id: Number(id) },
+      });
+
+      if (userToUpdate) {
+        await database.user.update(updateuser, {
+          where: { id: Number(id) },
+        });
+
+        return updateuser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getUser(id) {
+    try {
+      const theuser = await database.user.findOne({
+        where: { id: Number(id) },
+      });
+
+      return theuser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteUser(id) {
+    try {
+      const userToDelete = await database.user.findOne({
+        where: { id: Number(id) },
+      });
+
+      if (userToDelete) {
+        const deleteduser = await database.user.destroy({
+          where: { id: Number(id) },
+        });
+        return deleteduser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default userService;
