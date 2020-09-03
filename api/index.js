d0b4b86b18803661593897c2e4ec3c38bb5e2bf0
naereloire <naere.loire@gmail.com>
2020-09-02 21:04:37 -0300

    add routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './server/routes/userRoutes';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
+app.use('/api/users', userRoutes);
 
 app.get('*', (req, res) =>
   res.status(200).send('<html><h1>Hello World!</h1></html>'),
@@ -14,3 +16,4 @@ app.get('*', (req, res) =>
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
 });
+module.exports = app;
