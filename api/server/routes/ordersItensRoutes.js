9dff24c4b5182e7e8d5125380e90c19cedccdaf9
naereloire <naere.loire@gmail.com>
2020-09-05 18:51:52 -0300

    documentacao rota orderItens

--- api/server/routes/ordersItensRoutes.js
+++ api/server/routes/ordersItensRoutes.js
@@ -7,20 +7,35 @@ router.get(
   '/ordersItens/',
   ordersItensController.getAllOrderItens,
   // #swagger.tags = ["Orders Itens"]
-  // #swagger.description="testando desrição"
+  // #swagger.description="Essa rota realiza consulta, de todos os itens de pedido na base de dados."
+  /* #swagger.responses[200] = { 
+               description: 'Itens do pedido não encontrados.' 
+        } */
+  /* #swagger.responses[400] = { 
+               description: 'Requisição Inválida.' 
+        } */
 );
 
 router.get(
   '/ordersItens/:id',
   ordersItensController.getOrderItem,
   // #swagger.tags = ["Orders Itens"]
-  // #swagger.description="testando desrição"
+  // #swagger.description="Essa rota realiza consulta, de um item em um pedido através do **_id_**."
+  /* #swagger.responses[200] = { 
+               description: 'Item do pedido Encontrado.' 
+        } */
+  /* #swagger.responses[400] = { 
+               description: 'Requisição Inválida.' 
+        } */
+  /* #swagger.responses[404] = { 
+               description: 'Item do pedido não Encontrado.' 
+        } */
 );
 router.post(
   '/ordersItens/add',
   ordersItensController.addOrderItem,
   // #swagger.tags = ["Orders Itens"]
-  // #swagger.description="testando desrição"
+  // #swagger.description="Essa rota realiza criação, de um item à um pedido conforme **_newItem:_**."
   /* #swagger.parameters['newItem'] = {
                in: 'body',
                description: 'Informações do Item.',
@@ -29,12 +44,18 @@ router.post(
                schema: { $ref: "#/definitions/AddItemCount" }
         } 
   */
+  /* #swagger.responses[201] = { 
+               description: 'Item Criado.' 
+        } */
+  /* #swagger.responses[400] = { 
+               description: 'Requisição Inválida.' 
+        } */
 );
 router.put(
   '/order/update/count/:id',
   ordersItensController.updateItensCount,
   // #swagger.tags = ["Orders Itens"]
-  // #swagger.description="testando desrição"
+  // #swagger.description="Essa rota realiza atualização, na quantidade de item em um pedido conforme **_updateItemCount:_**."
   /* #swagger.parameters['updateItemCount'] = {
                in: 'body',
                description: 'Alteração de quantidade do item.',
@@ -43,13 +64,31 @@ router.put(
                schema: { count:2}
         } 
   */
+  /* #swagger.responses[200] = { 
+               description: 'Quantidade Atualizada.' 
+        } */
+  /* #swagger.responses[400] = { 
+               description: 'Requisição Inválida.' 
+        } */
+  /* #swagger.responses[404] = { 
+               description: 'Item não Encontrado.' 
+        } */
 );
 
 router.delete(
   '/ordersItens/:id',
   ordersItensController.deleteItem,
   // #swagger.tags = ["Orders Itens"]
-  // #swagger.description="testando desrição"
+  // #swagger.description="Essa rota realiza exclusão, de um item em um pedido."
+  /* #swagger.responses[200] = { 
+               description: 'Item do pedido Excluído.' 
+        } */
+  /* #swagger.responses[400] = { 
+               description: 'Requisição Inválida.' 
+        } */
+  /* #swagger.responses[404] = { 
+               description: 'Item do pedido não Encontrado.' 
+        } */
 );
 
 export default router;
